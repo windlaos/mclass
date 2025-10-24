acadc06b75f3b261a38c078f6473b3d41b72f88e
windlaos <cwjang701014@gmail.com>
2025-10-25 08:39:31 +0900

    add

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -4,17 +4,26 @@ import com.example.demo.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
 
-    private final ProductService productService;
-
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("products", productService.findAll());
+    public String home() {
         return "index";
+    }
+
+    @GetMapping("/company")
+    public String company() {
+        return "company";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
     }
 }
