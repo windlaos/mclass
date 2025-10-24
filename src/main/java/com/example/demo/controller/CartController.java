07967a5c27cefa2ea8bf774756e3618670a7aea1
windlaos <cwjang701014@gmail.com>
2025-10-25 07:39:35 +0900

    add code

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class CartController {
+
+    private final ProductService productService;
 
     @GetMapping("/cart/add/{id}")
     public String addToCart(@PathVariable Long id) {
-        System.out.println("Cart Add Clicked! productId=" + id);
-        return "redirect:/"; // 임시 처리: 홈 이동
+        // TODO : 추후 세션 장바구니 기능 추가 예정
+        System.out.println("장바구니 담기 성공: 상품 ID = " + id);
+
+        return "redirect:/"; // ✅ 일단 메인으로 이동시키기
     }
 }
