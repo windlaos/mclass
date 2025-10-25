767455803bcace14669fb4abb280b35f9397ab0a
windlaos <cwjang701014@gmail.com>
2025-10-25 19:34:00 +0900

    add

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.service.ProductService;
 
