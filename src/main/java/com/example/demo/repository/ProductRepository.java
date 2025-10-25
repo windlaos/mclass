ce818e2b773283e2a7ac3fc2256ea72ab2587625
windlaos <cwjang701014@gmail.com>
2025-10-25 09:11:08 +0900

    add

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Product;
+import com.example.demo.entity.Product; // ✅ 이걸로 변경
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
