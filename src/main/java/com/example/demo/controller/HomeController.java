02a9d4a5959a708719ffb88789e6033930acbd50
windlaos <cwjang701014@gmail.com>
2025-10-25 19:23:52 +0900

    add

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";  // 상품 목록 페이지
+    public String home(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "index";
     }
 }
 
