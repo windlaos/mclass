e872e634fdcd66f5cce4840df812f15340585093
windlaos <cwjang701014@gmail.com>
2025-10-27 21:02:06 +0900

    add

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -6,14 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    /**
+     * 회사 소개 페이지
+     */
     @GetMapping("/company")
-    public String company() {
-        return "company";
+    public String showCompanyPage() {
+        return "company"; // templates/company.html
     }
 
+    /**
+     * 개인정보 처리방침 페이지
+     */
     @GetMapping("/privacy")
-    public String privacy() {
-        return "privacy";
+    public String showPrivacyPolicyPage() {
+        return "privacy"; // templates/privacy.html
     }
 }
-
