1b762a99e6e038add472ea982588c32411aa91e4
Chaeweon Jang <cwjang701014@gmail.com>
2025-10-27 09:29:30 +0900

    add

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,25 +1,21 @@
-package com.example.demo.controller;
+ppackage com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.service.ProductService;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
     private final ProductService productService;
 
-    @Autowired
-    public HomeController(ProductService productService) { // ✅ 생성자 주입 권장
-        this.productService = productService;
-    }
-
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("products", productService.productService.findAll());
+        model.addAttribute("products", productService.findAll());
         return "home";
     }
 }
