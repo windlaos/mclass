12dce0b2ee5d9396d97b467880f9eecb6df210a4
windlaos <cwjang701014@gmail.com>
2025-10-27 22:35:51 +0900

    add

--- src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -1,9 +1,13 @@
-package com.example.demo.config;
+package com.example.demo.service;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,16 +17,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username)
-            .orElseThrow(() ->
-                new UsernameNotFoundException("user not found: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다: " + username));
 
-        return User.withUsername(user.getUsername())
-                .password(user.getPassword())
-                .roles(user.getRole().substring(5)) // ROLE_ADMIN → ADMIN
-                .build();
+        UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(user.getUsername());
+        builder.password(user.getPassword());
+        builder.roles(user.getRole());
+
+        return builder.build();
     }
 }
